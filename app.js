c091732ea33ef6d33d6f69d5306e90cba1f161e8
Daniel <danstraw86@gmail.com>
2018-10-06 08:31:08 -0700

    hard coded a jquery file into assets/scripts

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const $ = require('jquery')
-
 $('#year').text(new Date().getFullYear())
 
 $('.carousel').carousel({
-  interval: 500,
+  interval: 5000,
   keyboard: true,
   pause: 'hover',
   wrap: true
